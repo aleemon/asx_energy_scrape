492172e657f30a3824371377127db0636650593e
aleemon <69752670+aleemon@users.noreply.github.com>
2023-01-10 22:01:39 +1100

    Create postprocess.ts

---
+++ .github/workflows/postprocess.ts
@@ -0,0 +1,14 @@
+// 1. Install necessary packages
+// Might also need ggplot for what I want?
+const r_install = Deno.run({
+    cmd: ['sudo', 'Rscript', '-e', "install.packages(c('dplyr', 'readr', 'lubridate', 'stringr'))"]
+});
+
+await r_install.status();
+
+// 2. Forward the execution to the R script
+const r_run = Deno.run({
+    cmd: ['Rscript', './clean.R']
+});
+
+await r_run.status();
