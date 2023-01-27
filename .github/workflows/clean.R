95dbbc64ed4af448cc5968f6292903eb27fa9c77
aleemon <69752670+aleemon@users.noreply.github.com>
2023-01-27 16:19:18 +1100

    Update clean.R

--- .github/workflows/clean.R
+++ .github/workflows/clean.R
@@ -10,15 +10,13 @@ asx_trades <- readxl::read_excel("./asx_trades.xls")
 # Add column names
 colnames(asx_trades) <- c("time_AEDT", "code", "volume", "price_$_MWh")
 
-# Should probably do some further processing here.
+# Add in a date column
+asx_trades$date <-as.Date(Sys.Date())
 
-# All the processing!
-#clean_data <- raw_data %>%
-#  mutate(Zipcode = as.character(Zipcode),
-#         Year = lubridate::year(Date),
-#         Sex = ifelse(is.na(Sex), 'Unknown', Sex))
+# Should probably do some further processing here.
 
 
 
 # Output data
+#readr::write_csv(clean_data, paste0("asx_trades_", Sys.Date(),"./output.csv")) # Not sure if this will work
 readr::write_csv(clean_data, "./output.csv")
