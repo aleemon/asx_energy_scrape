fb82afbb1125bbfdef798f04d723fbfe0c78952b
aleemon <69752670+aleemon@users.noreply.github.com>
2023-10-12 22:23:58 +1100

    Update clean.R

--- clean.R
+++ clean.R
@@ -9,15 +9,15 @@ new_trades <- read.delim("asx_trades.xls")
 
 colnames(new_trades) <- c("time_AEDT", "code", "volume", "price_$_MWh")
 
-asx_trades$date <-as.Date(Sys.Date())
+new_trades$date <-as.Date(Sys.Date())
 
 #~ Add in the longer form descriptions of the commodity codes
 
 # Output new data
   #~ Move this a different folder
-write.csv(asx_trades, file = paste0("asx_trades_", Sys.Date(),"_output.csv")) # Not sure if this will work
+write.csv(new_trades, file = paste0("asx_trades_", Sys.Date(),"_output.csv")) # Not sure if this will work
 
-write.csv(asx_trades, file = paste0("./daily_files/asx_trades_", Sys.Date(),"_output.csv")) # Not sure if this will work
+write.csv(new_trades, file = paste0("./daily_files/asx_trades_", Sys.Date(),"_output.csv")) # Not sure if this will work
 
 
 
