ed7f5b3e0c6e40ec07ce2095cb2f9ff50ab9a519
aleemon <69752670+aleemon@users.noreply.github.com>
2023-10-12 22:13:48 +1100

    Update clean.R

--- clean.R
+++ clean.R
@@ -5,19 +5,24 @@
 #library(lubridate)
 
 # Read in data, with the same name that we specified in `flat.yml`
-asx_trades <- read.delim("asx_trades.xls")
-# It's a tab delimited file, not an xls file. Thanks cockheads.
+new_trades <- read.delim("asx_trades.xls")
 
-# Add column names
-colnames(asx_trades) <- c("time_AEDT", "code", "volume", "price_$_MWh")
+colnames(new_trades) <- c("time_AEDT", "code", "volume", "price_$_MWh")
 
-# Add in a date column
 asx_trades$date <-as.Date(Sys.Date())
 
-# Should probably do some further processing here.
+#~ Add in the longer form descriptions of the commodity codes
 
-
-
-# Output data
+# Output new data
+  #~ Move this a different folder
 write.csv(asx_trades, file = paste0("asx_trades_", Sys.Date(),"_output.csv")) # Not sure if this will work
-#readr::write_csv(clean_data, "./output.csv")
+
+write.csv(asx_trades, file = paste0("./daily_files/asx_trades_", Sys.Date(),"_output.csv")) # Not sure if this will work
+
+
+
+perpetual_trades <- read.csv("./perpetual_file/perpetual_trades.csv")
+
+#perpetual_trades <- rbind(pertetual_trades, new_trades)
+
+#write.csv(perpetual_trades, file = "./perpetual_file/perpetual_trades.csv")
