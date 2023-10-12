214821015381c955b120bd9b3cb636ac07c02810
aleemon <69752670+aleemon@users.noreply.github.com>
2023-10-12 17:08:52 +1100

    Update clean.R
    Fixed reading/parsing error

--- clean.R
+++ clean.R
@@ -1,11 +1,12 @@
 # Load libraries
 #library(dplyr)
-library(readxl)
+#library(readxl)
 #library(stringr)
 #library(lubridate)
 
 # Read in data, with the same name that we specified in `flat.yml`
-asx_trades <- readxl::read_xls("asx_trades.xls")
+asx_trades <- read.delim("asx_trades.xls")
+# It's a tab delimited file, not an xls file. Thanks cockheads.
 
 # Add column names
 colnames(asx_trades) <- c("time_AEDT", "code", "volume", "price_$_MWh")
