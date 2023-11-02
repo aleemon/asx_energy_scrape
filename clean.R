7810c8d7239b94bfce7048020d3ac064162737ca
aleemon <69752670+aleemon@users.noreply.github.com>
2023-11-02 20:49:40 +1100

    Update clean.R

--- clean.R
+++ clean.R
@@ -2,7 +2,7 @@
 # Read in data, with the same name that we specified in `flat.yml`
 new_trades <- read.delim("asx_trades.xls")
 
-colnames(new_trades) <- c("time_AEDT", "code", "volume", "price_$_MWh")
+colnames(new_trades) <- c("time_aedt", "code", "volume", "price_doll_mwh")
 
 new_trades$date <-as.Date(Sys.Date())
 
@@ -10,9 +10,9 @@ new_trades$date <-as.Date(Sys.Date())
 
 # Output new data
   #~ Move this a different folder
-write.csv(new_trades, file = paste0("asx_trades_", Sys.Date(),"_output.csv")) # Not sure if this will work
+write.csv(new_trades, file = paste0("asx_trades_", Sys.Date(),"_output.csv"), row.names = FALSE)
 
-write.csv(new_trades, file = paste0("./daily_files/asx_trades_", Sys.Date(),"_output.csv")) # Not sure if this will work
+write.csv(new_trades, file = paste0("./daily_files/asx_trades_", Sys.Date(),"_output.csv"), row.names = FALSE)
 
 ## Temporary trick to overwrite the data and force datatype
 #perpetual_trades <- new_trades
@@ -22,4 +22,4 @@ perpetual_trades <- read.csv("./perpetual_file/perpetual_trades.csv")
 
 perpetual_trades <- rbind(perpetual_trades, new_trades)
 
-write.csv(perpetual_trades, file = "./perpetual_file/perpetual_trades.csv")
+write.csv(perpetual_trades, file = "./perpetual_file/perpetual_trades.csv", row.names = FALSE)
